2819a86c64338703ddd50accd614feeeb790db92
David F Smith <kittoes0124@gmail.com>
2016-03-27 18:58:38 -0500

    Added date cast.

--- math/Programmability/Table-valued/RandomDate.sql
+++ math/Programmability/Table-valued/RandomDate.sql
@@ -2,6 +2,6 @@
 returns table
 with schemabinding as
 return (
-    select DateAdd(day, RandomOffset.n, N'0001-01-01') as d
+    select DateAdd(day, RandomOffset.n, Cast(N'0001-01-01' as date)) as d
     from math.RandomIntBetween(0, 3652058) as RandomOffset
 );
